941b724db56ce9f93f62f59b540fccdb1342c6ca
Cvash <48260542+Cvash@users.noreply.github.com>
2021-07-05 17:44:06 -0500

    agreegando para llevar los cambios al main

--- src/login_bd/Login_bd.java
+++ src/login_bd/Login_bd.java
@@ -8,6 +8,7 @@ public class Login_bd {
     public static void main(String[] args) {
         
         //esto es para que inicialize desde el formulario login
+        //Frm_login.main(args);
         Frm_login.main(args);
  
     }
